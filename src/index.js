8dacb3b8622b4f43e4f5b26fb442e95b4f98943a
drk477 <61123555+drk477@users.noreply.github.com>
2021-11-02 19:38:00 -0300

    aula2-completa

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Hello World Ignite!" });
+  return res.json({ message: "Hello World Ignite - Fundamentos NodeJs" });
 });
 
 app.listen(3333);
