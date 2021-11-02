2f3d99d3f26aa3b1e81d3a6c73eb0047065ed951
drk477 <61123555+drk477@users.noreply.github.com>
2021-11-02 19:51:19 -0300

    aula-3 completa

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World Ignite - Fundamentos NodeJs" });
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 6", "Curso 3", "Curso 4"]);
+});
+
+app.delete("courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 6", "Curso 4"]);
 });
 
 app.listen(3333);
