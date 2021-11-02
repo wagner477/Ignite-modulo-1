3040f945731e0b6426d5ec9664b8bb1d2b161221
drk477 <61123555+drk477@users.noreply.github.com>
2021-11-02 20:30:23 -0300

    aula-5 completa

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
@@ -18,7 +26,7 @@ app.patch("/courses/:id", (req, res) => {
   return res.json(["Curso 6", "Curso 6", "Curso 3", "Curso 4"]);
 });
 
-app.delete("courses/:id", (req, res) => {
+app.delete("/courses/:id", (req, res) => {
   return res.json(["Curso 6", "Curso 6", "Curso 4"]);
 });
 
